13515142863f45101370b6a9fd5de05d064d6f93
Jayesh Rathi <itsjayeshrathi@gmail.com>
2025-04-18 09:14:07 +0530

    started learning golang

---
+++ go-beginner/go.mod
@@ -0,0 +1,3 @@
+module github.com/itsjayeshrathi/go-beginner
+
+go 1.24.2
---
+++ go-beginner/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name = "string"
+	fmt.Printf("Your name is %s\n", name)
+	fmt.Println("Hello World")
+
+	age := 29
+	fmt.Printf("Your age is %d\n", age)
+
+	var city string
+	city = "Seattle"
+	fmt.Printf("Your city is %s\n", city)
+
+	var country, continent = "USA", "North America"
+	fmt.Printf("this is my country %s, and this is my continent is %s\n", country, continent)
+
+	
+}
