7ee1c0a2d3bc213164ee77253f266ac5569cfd73
Jayesh Rathi <itsjayeshrathi@gmail.com>
2025-04-18 21:25:59 +0530

    wrote first route

---
+++ go-fem/go.mod
@@ -0,0 +1,3 @@
+module github.com/itsjayeshrathi/go-fem
+
+go 1.24.2
---
+++ go-fem/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/itsjayeshrathi/go-fem/internal/app"
+)
+
+func main() {
+	app, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("we are running our app")
+
+	http.HandleFunc("/healthcheck", HealthCheck)
+	
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
